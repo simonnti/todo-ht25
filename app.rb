f17baaa8211547f2b92a0237c22bcbd154a6f9ff
itggot-emil-willbas <emil@vildbas.com>
2025-11-19 14:28:14 +0100

    removed code from app.rb

--- app.rb
+++ app.rb
@@ -5,16 +5,7 @@ require 'sinatra/reloader'
 
 
 
-# Funktion för att prata med databasen
-# Exempel på användning: db.execute('SELECT * FROM fruits')
-def db
-  return @db if @db
 
-  @db = SQLite3::Database.new(DB_PATH)
-  @db.results_as_hash = true
-
-  return @db
-end
 
 # Routen /
 get '/' do
