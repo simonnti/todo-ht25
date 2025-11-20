7c4001027281a6a28ac5e7d90ec50444ecde6e2b
Simon Lendvai <simon.lendvai@elev.ga.ntig.se>
2025-11-20 16:31:04 +0100

    3rd
    yeah började o så

--- app.rb
+++ app.rb
@@ -3,13 +3,35 @@ require 'sqlite3'
 require 'slim'
 require 'sinatra/reloader'
 
-
-
-
-
-# Routen /
-get '/' do
-    slim(:index)
+post('/index/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/tasks.db")
+  db.execute("DELETE FROM tasks WHERE id = ?", id)
+  redirect('/index')
 end
 
+post('/task') do
+  db = SQLite3::Database.new("db/tasks.db")
+  db.results_as_hash = true
+  new_task = params[:new_task]
+  description = params[:description]
+  db.execute("INSERT INTO tasks (name, description) VALUES (?,?)",[new_fruit,description])
+  redirect('/index')
+end
 
+get('/index/:id/edit') do
+  db = SQLite3::Database.new("db/tasks.db")
+  db.results_as_hash = true
+  id = params[:id].to_i
+  @spestask = db.execute("SELECT * FROM tasks WHERE id = ?", [id]).first
+  slim(:"edit")
+end
+
+post('/index/:id/update') do
+  id = params[:id].to_i
+  name = params[:name]
+  description = params[:description]
+  db = SQLite3::Database.new("db/tasks.db")
+  db.execute("UPDATE tasks SET name=?, description=? WHERE id=?",[name,description,id])
+  redirect('/index')
+end
